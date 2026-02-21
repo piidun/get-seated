4cd58cf0663e0edae1b64f180615fc8ae041460e
le0-0 <leonard.schoyen@gmail.com>
2026-02-21 18:10:37 +0100

    Implement ensureLayoutValidForLevel

---
+++ source/abstract-experimentation/utility/ensureLayoutValidForLevel.ts
@@ -0,0 +1,31 @@
+import { Layout } from "../models/Layout.js";
+import { Level } from "../models/Level.js";
+import { Result } from "../models/Result.js";
+
+/**
+ * Ensure that the supplied {@link layout} is valid for the supplied {@link level}.
+ */
+export const ensureLayoutValidForLevel = (
+  layout: Layout,
+  level: Level,
+): Result => {
+  if (layout.passengerSeatIds.length !== level.passengers.length)
+    return {
+      success: false,
+      error: `The number of seatings (${
+        layout.passengerSeatIds.length
+      }) differ from the number of passengers (${level.passengers.length}).`,
+    };
+
+  const seatIdSet = level.stage.seatConnectionsMap.keySeq().toSet();
+
+  for (const seatId of layout.passengerSeatIds) {
+    if (!seatIdSet.has(seatId))
+      return {
+        success: false,
+        error: `Seat with id ${seatId} does not exist.`,
+      };
+  }
+
+  return { success: true };
+};
