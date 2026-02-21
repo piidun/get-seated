c30e902f70d1a646b94377dd817e39adbdf25132
May-Tove Hovdal <may-tove.hovdal@dfo.no>
2026-02-21 23:02:17 +0100

    Made it render dynamically

--- source/main.ts
+++ source/main.ts
@@ -1,19 +1,5 @@
-const container = document.querySelector(".character-container");
+import { levels } from "./data/levels.js";
+import { renderPassportCards } from "./render/passportCard.js";
 
-if (container) {
-  container.addEventListener("click", (e) => {
-    const target = e.target;
-
-    if (!(target instanceof Element)) return;
-
-    const card = target.closest(".passport-card");
-
-    if (!card) return;
-
-    document
-      .querySelectorAll(".passport-card")
-      .forEach((c) => c.classList.remove("selected"));
-
-    card.classList.add("selected");
-  });
-}
+let currentLevel = levels.level1; // later: pick based on game state
+renderPassportCards(currentLevel);
