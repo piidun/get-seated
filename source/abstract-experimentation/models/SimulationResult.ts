780736ef2c8f2248b8459bee145fe61114e967ca
le0-0 <leonard.schoyen@gmail.com>
2026-02-21 20:16:56 +0100

    Added finalComfortLevels to SimulationResult

--- source/abstract-experimentation/models/SimulationResult.ts
+++ source/abstract-experimentation/models/SimulationResult.ts
@@ -3,4 +3,5 @@ import { SimulationEvent } from "./SimulationEvent.js";
 export type SimulationResult = {
   readonly score: number;
   readonly events: readonly SimulationEvent[] | null;
+  readonly finalComfortLevels: readonly number[];
 };
