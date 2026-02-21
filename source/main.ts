3a9dae18cb77a9cb40667bbc1e031cf081f28823
May-Tove Hovdal <may-tove.hovdal@dfo.no>
2026-02-21 22:00:44 +0100

    Added selection logic on cards

--- source/main.ts
+++ source/main.ts
@@ -1,1 +1,19 @@
-console.log("Hello, World!");
+const container = document.querySelector(".character-container");
+
+if (container) {
+  container.addEventListener("click", (e) => {
+    const target = e.target;
+
+    if (!(target instanceof Element)) return;
+
+    const card = target.closest(".passport-card");
+
+    if (!card) return;
+
+    document
+      .querySelectorAll(".passport-card")
+      .forEach((c) => c.classList.remove("selected"));
+
+    card.classList.add("selected");
+  });
+}
