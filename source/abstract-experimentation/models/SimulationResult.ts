bfd91e6f706f904185c731177f1357c508fbe976
le0-0 <leonard.schoyen@gmail.com>
2026-02-21 20:42:04 +0100

    SimulationResult: Make events required.

--- source/abstract-experimentation/models/SimulationResult.ts
+++ source/abstract-experimentation/models/SimulationResult.ts
@@ -2,6 +2,6 @@ import { SimulationEvent } from "./SimulationEvent.js";
 
 export type SimulationResult = {
   readonly score: number;
-  readonly events: readonly SimulationEvent[] | null;
+  readonly events: readonly SimulationEvent[];
   readonly finalComfortLevels: readonly number[];
 };
