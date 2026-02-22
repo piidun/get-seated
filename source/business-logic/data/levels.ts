881fccbf9e53e0916daa7097abcabdfb4e503a7a
le0-0 <leonard.schoyen@gmail.com>
2026-02-22 16:39:58 +0100

    Added second level.

--- source/business-logic/data/levels.ts
+++ source/business-logic/data/levels.ts
@@ -1,12 +1,30 @@
 import { createStage } from "../logic/createStage";
 import { Level } from "../models/Level";
-import { PartyBroOne, PartyBroTwo } from "./passengers";
+import { passengers } from "./passengers";
 
 export const level1: Level = {
-  passengers: [PartyBroOne, PartyBroTwo],
+  passengers: [passengers.PartyBroOne, passengers.PartyBroTwo],
   stage: createStage(["SS_SS", "SS_SS"]),
+};
+
+export const level2: Level = {
+  passengers: [
+    passengers.BerryFieri,
+    passengers.DawnTewakemi,
+    passengers.HassatiniBladder,
+    passengers.InaMeeting,
+    passengers.LemmyTelya,
+    passengers.MaxInlove,
+    passengers.MaxineInlove,
+    passengers.PaigeTurner,
+    passengers.PartyBroOne,
+    passengers.PartyBroTwo,
+    passengers.TerryFied,
+  ],
+  stage: createStage(["SSS_SSS", "SSS_SSS"]),
 };
 
 export const levels = {
   level1,
+  level2,
 };
